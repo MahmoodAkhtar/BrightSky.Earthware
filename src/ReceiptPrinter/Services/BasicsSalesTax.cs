e5b62bdf0b3120c160d0a861578613fbea105f04
Mahmood Akhtar <mahmood233@hotmail.com>
2021-04-07 11:37:57 +0100

    Adding stub impls more to come

---
+++ src/ReceiptPrinter/Services/BasicsSalesTax.cs
@@ -0,0 +1,14 @@
+﻿using ReceiptPrinter.Domain;
+using ReceiptPrinter.Interfaces;
+using System;
+
+namespace ReceiptPrinter.Services
+{
+    public class BasicsSalesTax : ISalesTaxStratergy
+    {
+        public double Appy(BasketItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ReceiptPrinter/Services/GoodsClassifier.cs
@@ -0,0 +1,14 @@
+﻿using ReceiptPrinter.Domain;
+using ReceiptPrinter.Interfaces;
+using System;
+
+namespace ReceiptPrinter.Services
+{
+    public class GoodsClassifier : IGoodsClassifier
+    {
+        public GoodsType Classify(BasketItem item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
