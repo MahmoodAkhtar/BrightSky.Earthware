b5007a1f9f590da92d7686b24fe8e67a8a2a8ce6
Mahmood Akhtar <mahmood233@hotmail.com>
2021-04-07 11:44:57 +0100

    Adding stub impl of ISalesTaxRegime

---
+++ src/ReceiptPrinter/Services/SalesTaxRegime.cs
@@ -0,0 +1,27 @@
+﻿using ReceiptPrinter.Domain;
+using ReceiptPrinter.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace ReceiptPrinter.Services
+{
+    public class SalesTaxRegime : ISalesTaxRegime
+    {
+        private readonly List<ISalesTaxStratergy> _stratergies;
+
+        public SalesTaxRegime()
+        {
+            _stratergies = new List<ISalesTaxStratergy>();
+        }
+
+        public double Appy(BasketItem[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Register(ISalesTaxStratergy stratergy)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
