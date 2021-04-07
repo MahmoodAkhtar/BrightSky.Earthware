91d83f4d0dc15f18c2d6c9b66ed4ac357e6215e8
Mahmood Akhtar <mahmood233@hotmail.com>
2021-04-07 12:00:34 +0100

    Adding impl for sales tax stratergies

--- src/ReceiptPrinter/Services/BasicsSalesTax.cs
+++ src/ReceiptPrinter/Services/BasicsSalesTax.cs
@@ -6,9 +6,21 @@ namespace ReceiptPrinter.Services
 {
     public class BasicsSalesTax : ISalesTaxStratergy
     {
+        private readonly IGoodsClassifier _classifier;
+
+        public BasicsSalesTax(IGoodsClassifier classifier)
+        {
+            _classifier = classifier;
+        }
+
         public double Appy(BasketItem item)
         {
-            throw new NotImplementedException();
+            double tax = 0;
+
+            if (_classifier.Classify(item) == GoodsType.Unknown)
+                tax = item.Price * 0.1;
+
+            return tax;
         }
     }
 }
--- src/ReceiptPrinter/Services/ImportedSalesTax.cs
+++ src/ReceiptPrinter/Services/ImportedSalesTax.cs
@@ -5,19 +5,12 @@ namespace ReceiptPrinter.Services
 {
     public class ImportedSalesTax : ISalesTaxStratergy
     {
-        private readonly IGoodsClassifier _classifier;
-
-        public ImportedSalesTax(IGoodsClassifier classifier)
-        {
-            _classifier = classifier;
-        }
-
         public double Appy(BasketItem item)
         {
             double tax = 0;
 
-            if (_classifier.Classify(item) == GoodsType.Unknown)
-                tax = item.Price * 0.1;
+            if (item.IsImported)
+                tax = item.Price * 0.05;
 
             return tax;
         }
